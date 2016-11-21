f94d70bdfc9ed7d7c8db763de306cb40e7c9d8f7
WorstCoder <kuriozalnie@gmail.com>
2016-11-21 21:30:55 +0100

    Implemented filters and filters handler for better table view filtering

--- scraper/src/main/java/org/scraper/model/modles/ProxyModel.java
+++ scraper/src/main/java/org/scraper/model/modles/ProxyModel.java
@@ -1,95 +1,141 @@
 package org.scraper.model.modles;
 
 import javafx.application.Platform;
+import javafx.beans.Observable;
 import javafx.beans.property.ListProperty;
+import javafx.beans.property.ReadOnlyFloatWrapper;
 import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.util.Callback;
+import org.scraper.control.IClipboard;
+import org.scraper.control.IDeleter;
+import org.scraper.control.filters.*;
+import org.scraper.model.IConcurrent;
+import org.scraper.model.MainLogger;
 import org.scraper.model.Proxy;
 import org.scraper.model.checker.IProxyChecker;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 
-public class ProxyModel {
+public class ProxyModel implements IConcurrent, IDeleter<Proxy>, IClipboard<Proxy> {
 	
 	private IProxyChecker checker;
 	
-	private ObservableList<Proxy> visible = FXCollections.observableArrayList();
+	private ObservableList<Proxy> visible;
+	private ObservableList<Proxy> all = FXCollections.observableArrayList();
+	
+	private ListProperty<Proxy> allProperty = new SimpleListProperty<>(all);
+	
 	
 	private ObservableList<Proxy> working = FXCollections.observableArrayList();
 	private ListProperty<Proxy> workingProperty = new SimpleListProperty<>(working);
 	
-	private ObservableList<Proxy> all = FXCollections.observableArrayList();
-	private ListProperty<Proxy> allProperty = new SimpleListProperty<>(all);
+	private String proxyPattern = "[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}.*[0-9]";
 	
-	private boolean showBroken = false;
+	private Boolean showBroken = false;
 	
-	private Proxy.Type visibleType = Proxy.Type.ALL;
-	
-	private Proxy.Anonymity visibleAnonymity = Proxy.Anonymity.ALL;
+	private IFilter<Proxy.Anonymity, Proxy> anonymityFilter = new AnonymityFilter();
+	private IFilter<Proxy.Type, Proxy> typeFilter = new TypeFilter();
+	private IFilter<Float, Proxy> timeFilter = new TimeFilter();
+	private IFilter<Boolean, Proxy> workingFilter = new WorkingFilter();
+	private FiltersHandler<Proxy> filtersHandler = new FiltersHandler<>(anonymityFilter, typeFilter, timeFilter, workingFilter);
 	
 	public ProxyModel(IProxyChecker checker) {
 		this.checker = checker;
-	}
-	
-	
-	public void addProxy(Proxy proxy) {
-		Platform.runLater(() -> {
-			all.add(proxy);
-			
-			if (proxy.isWorking() || showBroken)
-				visible.add(proxy);
-			
-			if (proxy.isWorking())
-				working.add(proxy);
+		
+		Callback<Proxy, Observable[]> callback = (Proxy proxy) -> new Observable[]{new ReadOnlyFloatWrapper(proxy.getSpeed())};
+		visible = FXCollections.observableArrayList(callback);
+		
+		visible.addListener(new ListChangeListener<Proxy>() {
+			@Override
+			public void onChanged(Change<? extends Proxy> c) {
+				while (c.next()) {
+					if (c.wasUpdated()) {
+						MainLogger.log().info(" updated");
+					}
+				}
+			}
 		});
 	}
 	
+	public void addProxy(Proxy proxy) {
+		if (all.contains(proxy))
+			return;
+		
+		Platform.runLater(() -> {
+			all.add(proxy);
+			if (proxy.isWorking() || showBroken)
+				visible.add(proxy);
+		});
+	}
+	
+	public void addProxy(String proxyString) {
+		if (!proxyString.matches(proxyPattern)) return;
+		Proxy proxy = new Proxy(proxyString);
+		addProxy(proxy);
+	}
 	
 	public void check(List<Proxy> proxies) {
-		checker.checkProxies(proxies);
+		send(() ->
+					 checker.checkProxies(proxies), false);
 	}
 	
 	public void filterType(Proxy.Type type) {
+		typeFilter.setFilter(type);
+		Collection<Proxy> filtered = filtersHandler.filterList(all);
 		visible.clear();
-		visibleType = type;
-		all.forEach(proxy -> {
-			if ((proxy.getType() == type || type == Proxy.Type.ALL)
-					&& (proxy.getAnonymity() == visibleAnonymity || visibleAnonymity == Proxy.Anonymity.ALL)
-					&& (showBroken || proxy.isWorking()))
-				visible.add(proxy);
-		});
+		visible.addAll(filtered);
+		
 	}
 	
 	public void filterAnonymity(Proxy.Anonymity anonymity) {
+		anonymityFilter.setFilter(anonymity);
+		Collection<Proxy> filtered = filtersHandler.filterList(all);
 		visible.clear();
-		visibleAnonymity = anonymity;
-		
-		all.forEach(proxy -> {
-			if ((proxy.getAnonymity() == anonymity || anonymity == Proxy.Anonymity.ALL)
-					&& (proxy.getType() == visibleType || visibleType == Proxy.Type.ALL)
-					&& (showBroken || proxy.isWorking()))
-				visible.add(proxy);
-		});
+		visible.addAll(filtered);
 	}
 	
 	public void filterTimeout(Float time) {
+		timeFilter.setFilter(time);
+		Collection<Proxy> filtered = filtersHandler.filterList(all);
 		visible.clear();
-		all.forEach(proxy -> {
-			if (proxy.getSpeed() <= time)
-				visible.add(proxy);
-		});
+		visible.addAll(filtered);
 	}
 	
 	public void showBroken(boolean show) {
 		showBroken = show;
+		workingFilter.setFilter(show);
+		Collection<Proxy> filtered = filtersHandler.filterList(all);
 		visible.clear();
-		
-		all.forEach(proxy -> {
-			if (proxy.isWorking() || showBroken)
-				visible.add(proxy);
-		});
+		visible.addAll(filtered);
+	}
+	
+	@Override
+	public void deleteSelected(List<Proxy> selected) {
+		for (Proxy proxy : new CopyOnWriteArrayList<>(selected)) {
+			visible.remove(proxy);
+			all.remove(proxy);
+		}
+	}
+	
+	@Override
+	public void handleCopy(List<Proxy> proxyList) {
+		String selectedProxiesString = proxyList.stream()
+				.map(Proxy::getIpPort)
+				.collect(Collectors.joining("\n"));
+		copy(selectedProxiesString);
+	}
+	
+	@Override
+	public void handlePaste() {
+		List<String> clipboardContent = paste();
+		clipboardContent.forEach(this::addProxy);
 	}
 	
 	public ObservableList<Proxy> getVisible() {
--- scraper/src/main/java/org/scraper/model/modles/SitesModel.java
+++ scraper/src/main/java/org/scraper/model/modles/SitesModel.java
@@ -3,18 +3,28 @@ package org.scraper.model.modles;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import org.scraper.model.IPool;
-import org.scraper.model.MainPool;
-import org.scraper.model.managers.AssignManager;
+import org.scraper.control.IClipboard;
+import org.scraper.control.IDeleter;
+import org.scraper.control.filters.AvgSitesFilter;
+import org.scraper.control.filters.AvgWorkingFilter;
+import org.scraper.control.filters.FiltersHandler;
+import org.scraper.control.filters.IFilter;
+import org.scraper.model.IConcurrent;
 import org.scraper.model.gather.LinksGather;
+import org.scraper.model.managers.AssignManager;
 import org.scraper.model.scrapers.ProxyScraper;
+import org.scraper.model.scrapers.ScrapeType;
 import org.scraper.model.web.IDataBase;
+import org.scraper.model.web.LanguageCheck;
 import org.scraper.model.web.Site;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 
-public class SitesModel {
+public class SitesModel implements IConcurrent, IDeleter<Site>, IClipboard<Site> {
 	
 	private ProxyScraper scraper;
 	
@@ -22,38 +32,93 @@ public class SitesModel {
 	
 	private LinksGather gather;
 	
-	private IDataBase IDataBase;
+	private IDataBase dataBase;
 	
 	private ObservableList<Site> visible = FXCollections.observableArrayList();
+	private List<Site> all = new ArrayList<>();
 	
-	public SitesModel(AssignManager assigner, ProxyScraper scraper, LinksGather gather, IDataBase IDataBase) {
+	private IFilter<Integer, Site> avgSitesFilter = new AvgSitesFilter();
+	private IFilter<Integer, Site> avgWorkingFilter = new AvgWorkingFilter();
+	private FiltersHandler<Site> filtersHandler = new FiltersHandler<>(avgSitesFilter, avgWorkingFilter);
+	
+	private static String sitePattern = ".*\\.[a-z]{2,3}.*";
+	
+	public SitesModel(AssignManager assigner, ProxyScraper scraper, LinksGather gather, IDataBase dataBase) {
 		this.assigner = assigner;
 		this.scraper = scraper;
 		this.gather = gather;
-		this.IDataBase = IDataBase;
+		this.dataBase = dataBase;
 	}
 	
-	public void addSite(Site site){
+	public void addSite(Site site) {
+		if (all.contains(site) || LanguageCheck.isFromOtherLang(site, all)) return;
+		all.add(site);
+		
 		Platform.runLater(() -> visible.add(site));
 	}
 	
-	public ObservableList<Site> getVisible(){
+	public void addSite(String siteString) {
+		if (!siteString.matches(sitePattern)) return;
+		Site site = new Site(siteString, ScrapeType.UNCHECKED);
+		addSite(site);
+	}
+	
+	public ObservableList<Site> getVisible() {
 		return visible;
 	}
 	
 	public void scrape(List<Site> sites) {
-		scraper.scrapeList(sites);
+		send(() ->
+					 scraper.scrapeList(sites), false);
 	}
 	
 	public void check(List<Site> sites) {
-		MainPool.getInstance().sendTask(() ->{
-		assigner.assignList(sites);
-		IDataBase.postSites(new ArrayList<>(sites));
-		}, false);
+		send(() ->
+			 {
+				 assigner.assignList(sites);
+				 //dataBase.postSites(new ArrayList<>(sites));
+			 }, false);
 	}
 	
-	public void gather(List<Site> sites, Integer depth){
-		gather.setDepth(depth);
-		gather.gatherList(sites);
+	public void gather(List<Site> sites, Integer depth) {
+		send(() ->
+			 {
+				 gather.setDepth(depth);
+				 gather.gatherList(sites);
+			 }, false);
+	}
+	
+	public void filterAvgSites(Integer minSites){
+		avgSitesFilter.setFilter(minSites);
+		Collection<Site> filtered = filtersHandler.filterList(all);
+		visible.clear();
+		visible.addAll(filtered);
+	}
+	
+	public void filterAvgWorking(Integer minWorking){
+		avgWorkingFilter.setFilter(minWorking);
+		Collection<Site> filtered = filtersHandler.filterList(all);
+		visible.clear();
+		visible.addAll(filtered);
+	}
+	
+	@Override
+	public void deleteSelected(List<Site> selected) {
+		for (Site site : new CopyOnWriteArrayList<>(selected)){
+			visible.remove(site);}
+	}
+	
+	@Override
+	public void handleCopy(List<Site> proxyList) {
+		String selectedProxiesString = proxyList.stream()
+				.map(Site::getAddress)
+				.collect(Collectors.joining("\n"));
+		copy(selectedProxiesString);
+	}
+	
+	@Override
+	public void handlePaste() {
+		List<String> clipboardContent = paste();
+		clipboardContent.forEach(this::addSite);
 	}
 }

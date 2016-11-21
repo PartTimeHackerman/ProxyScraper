3871cdb58756a84d3e647582ccda33b265b73db6
WorstCoder <kuriozalnie@gmail.com>
2016-11-21 21:05:48 +0100

    Created filters and filters handler for better table view filtering

---
+++ scraper/src/main/java/org/scraper/control/filters/IFilter.java
@@ -0,0 +1,10 @@
+package org.scraper.control.filters;
+
+import java.util.Collection;
+
+public interface IFilter<F, E> {
+	
+	Collection<E> filterList(Collection<E> listToFilter);
+	
+	void setFilter(F filter);
+}

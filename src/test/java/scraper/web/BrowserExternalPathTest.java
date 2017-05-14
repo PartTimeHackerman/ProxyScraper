cd7c01eb83ef1e9013c7b07cc5ccf3db57345896
WorstCoder <kuriozalnie@gmail.com>
2017-05-15 00:01:13 +0200

    Added PhantomJS path setter test

---
+++ src/test/java/scraper/web/BrowserExternalPathTest.java
@@ -0,0 +1,31 @@
+package scraper.web;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+
+import static org.junit.Assert.assertNotNull;
+
+public class BrowserExternalPathTest {
+	
+	private static Browser browser;
+	
+	@BeforeClass
+	public static void setUp() throws Exception {
+		Browser.setPhantomJsPath("D:/JavaProjects/ProxyScraper/ScraperBuild/phantomjs.exe");
+		browser = new Browser();
+		assertNotNull(browser.getDriver());
+	}
+	
+	@Test
+	public void getPID() throws Exception {
+		Long pid = browser.getPID((PhantomJSDriver) browser.getDriver());
+		assertNotNull(pid);
+	}
+	
+	@AfterClass
+	public static void shutdown() throws Exception {
+		browser.shutdown();
+	}
+}
